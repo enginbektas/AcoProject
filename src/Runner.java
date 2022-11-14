04f4411ab5ee452c1e59036b1bb940d97c9bf9c5
enginbektas <enginbkts97@hotmail.com>
2022-11-14 10:57:53 +0300

    initial commit

---
+++ src/Runner.java
@@ -0,0 +1,6 @@
+public class Runner {
+    public static void main(String[] args) {
+        AntColonyOptimization antColony = new AntColonyOptimization(100);
+        antColony.startAntOptimization();
+    }
+}
